2923cc7443d1a8b6c71bab1f8b7a5c545ab523b6
ImYichus <s22120205@alumnos.itsur.edu.mx>
2025-09-18 00:02:36 -0600

    VERSION 3.0

--- app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -31,14 +31,16 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.Row
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-
-        Text("You've had $count glasses.")
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one")
         }
     }
